0adca219d71ae13c0f1bec3ad848b5b5a9457951
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-07-06 20:56:18 +0530

    address map here as well

--- src/parser.rs
+++ src/parser.rs
@@ -33,7 +33,7 @@ impl Map {
     let _ = fs::create_dir_all("./db/res");
 
     let mut file = File::create("./db/map/1.json").unwrap();
-    let _ = file.write(b"{");
+    let _ = file.write(b"[");
 
     let mut search = File::create("./db/search/1.json").unwrap();
     let _ = search.write(b"[");
@@ -49,7 +49,7 @@ impl Map {
   fn close_file(&mut self) {
     let _ = self.search.write_all(b"]");
     let _ = self.search.flush();
-    let _ = self.c_file.write_all(b"}");
+    let _ = self.c_file.write_all(b"]");
     let _ = self.c_file.flush();
   }
 
@@ -59,7 +59,7 @@ impl Map {
     self.close_file();
 
     let mut map = File::create("./db/map/1.json").unwrap();
-    let _ = map.write(b"{");
+    let _ = map.write(b"[");
 
     let mut search = File::create("./db/map/1.json").unwrap();
     let _ = search.write(b"[");
@@ -91,12 +91,12 @@ impl Map {
 
     let _ = self
       .c_file
-      .write(format!("\"{}\":\"l:{}\"", app.appDisplayName, app.appId).as_bytes());
+      .write(format!("\"l:{}\"", app.appId).as_bytes());
     let _ = self.search.write(
       format!(
-        "{{\"name\": {:?}, \"title\": {:?}, \"id\": {:?}}}",
-        app.appDisplayName,
-        app.appShortcutName,
+        "{{\"name\": {}, \"title\": {}, \"id\": {:?}}}",
+        serde_json::to_string(&app.appDisplayName).unwrap(),
+        serde_json::to_string(&app.appShortcutName).unwrap(),
         format!("l:{}", app.appId)
       )
       .as_bytes(),
