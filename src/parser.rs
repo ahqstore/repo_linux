9096ebb4f1a6cb07ec168ae19b889efc02bfd88b
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-10-05 21:18:03 +0530

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -218,7 +218,7 @@ async fn parse_push(app: ParsedApp, map: &mut Map) -> Option<()> {
   }
 
   let app = AHQStoreApplication {
-    usrVersion: version,
+    usrVersion: Some(version),
     version: 0,
     appDisplayName: app.name.clone(),
     appShortcutName: app.name.clone(),
